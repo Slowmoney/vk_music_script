549546b052cb584e2e82e37391b0750f20b2d7cd
Slowmoney <frolenkov58@gmail.com>
2020-06-05 20:03:28 +0300

    Update vk_music_download+tg+ds.js

--- vk_music_download+tg+ds.js
+++ vk_music_download+tg+ds.js
@@ -6,24 +6,8 @@
 // @include     http://vk.com/*
 // @include     https://vk.com/*
 // @run-at      document-end
-// @grant       GM_listValues
-// @grant       GM_setValue
-// @grant       GM_getValue
-// @grant       GM_deleteValue
 // @grant       GM_xmlhttpRequest
-// @grant       GM_info
-// @grant       GM_openInTab
-// @grant       GM_setClipboard
-// @grant       GM_registerMenuCommand
-// @grant       GM_unregisterMenuCommand
-// @grant       GM_notification
 // @grant       GM_download
-// @grant       GM.info
-// @grant       GM.listValues
-// @grant       GM.setValue
-// @grant       GM.getValue
-// @grant       GM.deleteValue
-// @grant       GM.openInTab
 // @grant       GM.setClipboard
 // @grant       GM.xmlHttpRequest
 
