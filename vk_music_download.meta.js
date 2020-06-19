6410f34cc46335c28cb7e183ab2e557cb034664b
Slowmoney <frolenkov58@gmail.com>
2020-06-19 14:53:04 +0300

    upd: user script auto upd

---
+++ vk_music_download.meta.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name Vk music downloader
+// @description:ru Кнопки для скачивания музыки
+// @namespace Slowmoney
+// @version     19.06.2020
+// @downloadUrl   https://raw.githubusercontent.com/Slowmoney/vk_music_script/master/vk_music_download.user.js
+// @updateUrl     https://raw.githubusercontent.com/Slowmoney/vk_music_script/master/vk_music_download.meta.js
+// @match       *://vkontakte.ru/*
+// @match       *://*.vkontakte.ru/*
+// @match       *://vk.com/*
+// @match       *://*.vk.com/*
+// @match       *://userapi.com/*
+// @match       *://*.userapi.com/*
+// @match       *://vk.me/*
+// @match       *://*.vk.me/*
+// @match       *://*.vkuseraudio.net/*
+// @match       *://*.vkuservideo.net/*
+// @connect     self
+// @connect     vkontakte.ru
+// @connect     vk.com
+// @connect     userapi.com
+// @connect     vk.me
+// @connect     vkuseraudio.net
+// @connect     vkuservideo.net
+// @run-at       document-start
+// @grant        unsafeWindow
+// @grant        GM_xmlhttpRequest
+// @grant        GM_download
+// @grant        GM_setValue
+// @grant        GM_getValue
+// ==/UserScript==
--- vk_music_download.user.js
+++ vk_music_download.user.js
@@ -2,15 +2,32 @@
 // @name Vk music downloader tg playlist discord
 // @description:ru Кнопки для скачивания музыки
 // @namespace Slowmoney
-// @version     05.06.2020
-// @include     http://vk.com/*
-// @include     https://vk.com/*
-// @run-at      document-end
-// @grant       GM_xmlhttpRequest
-// @grant       GM_download
-// @grant       GM.setClipboard
-// @grant       GM.xmlHttpRequest
-
+// @version     19.06.2020
+// @downloadUrl   https://raw.githubusercontent.com/Slowmoney/vk_music_script/master/vk_music_download.user.js
+// @updateUrl     https://raw.githubusercontent.com/Slowmoney/vk_music_script/master/vk_music_download.meta.js
+// @match       *://vkontakte.ru/*
+// @match       *://*.vkontakte.ru/*
+// @match       *://vk.com/*
+// @match       *://*.vk.com/*
+// @match       *://userapi.com/*
+// @match       *://*.userapi.com/*
+// @match       *://vk.me/*
+// @match       *://*.vk.me/*
+// @match       *://*.vkuseraudio.net/*
+// @match       *://*.vkuservideo.net/*
+// @connect     self
+// @connect     vkontakte.ru
+// @connect     vk.com
+// @connect     userapi.com
+// @connect     vk.me
+// @connect     vkuseraudio.net
+// @connect     vkuservideo.net
+// @run-at       document-start
+// @grant        unsafeWindow
+// @grant        GM_xmlhttpRequest
+// @grant        GM_download
+// @grant        GM_setValue
+// @grant        GM_getValue
 // ==/UserScript==
 (function (window, undefined) {
 	let w;
